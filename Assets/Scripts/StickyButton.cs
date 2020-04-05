af57bf7efc825a4dee7d70ec4431aa9e1c96c96c
Teo G Nistor <teo.g.nistor@gmail.com>
2020-04-05 18:16:09 +0100

    buton

---
+++ Assets/Scripts/StickyButton.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+[RequireComponent(typeof(Button))]
+public class StickyButton : MonoBehaviour {
+    [SerializeField] private int factionIndex;
+    [SerializeField] private Sprite pressed;
+    private Sprite released;
+
+    private Image image;
+
+    void Start () {
+        image = GetComponent<Image>();
+        released = image.sprite;
+        image.sprite = Game.IsFactionActive(factionIndex) ? pressed : released;
+    }
+    
+    public void Press () {
+        image.sprite = Game.PressButtonForFaction(factionIndex) ? pressed : released;
+    }
+}
